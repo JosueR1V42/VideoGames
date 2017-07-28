344ff4da5621967c18b1eac002cfbc5a699bd3f3
Josue David Rivas Velasquez <davixback@gmail.com>
2017-07-28 14:16:10 -0600

    actualizacion

--- VideoGames/Juegos/models.py
+++ VideoGames/Juegos/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
-
 # Create your models here.
-class creator(model.Model):
+class creator(models.Model):
+	generes=(
+			('F','Female'),
+			('M','Male'),
+		)
+    name=models.CharField(max_lenght=25,help_text='Nombre del creador ')
+	birthday=models.DateField(help_text='Fecha de nacimiento')
+	genere=models.Charfield(max_lenght=1, help_text='Coloque el sexo del creador' choices=generes)
 
-class Juego(models.Model):
+class company(models.Model):
+	name=models.Charfield(max_lenght=30,help_text='Compañia Desarrolladora')
+
+	
+class games(models.Model):
     """
     almacena los juegos mediante el equipo de
     administracion
     """
-    opinion=models.CharField(max_lenght=50,help_text='Ingrese el Titulo del juego a ingresar: ')
     title=models.CharField(max_lenght=30, help_text='Titulo del videojuego ')
     creatores=models.ForeignKey(creator,help_text='Nombre del o los creadores ')
     synopsis=models.TextField(max_lenght=300, help_text='Historia del Videojuego ')
@@ -18,6 +27,8 @@ class Juego(models.Model):
     genere=models.CharField(max_lenght=50, help_text='Genero del videojuego ')
     console=models.CharField(max_lenght=50, help_text='Plataforma de videojuego ')
 
+
 class review(models.Model):
-    review=models.IntegerField(max_lenght=1,help_text='Opiniones ')
-	game=models.ForeignKey(Juego,help_text='Selecione el juego que desea hacer la sinopsis')
+    review=models.IntegerField(max_lenght=100,help_text='Opiniones ')
+	game=models.ForeignKey(games,help_text='Selecione el juego que desea hacer la sinopsis')
+	
