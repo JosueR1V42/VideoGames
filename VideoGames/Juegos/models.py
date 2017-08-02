cde835d6dff5c04d001d092d27c90cd0b33a2025
Edu Santos Paz <eduardo@edsantoshn.com>
2017-08-01 20:24:24 -0600

    Identado
    Correción de identado

--- VideoGames/Juegos/models.py
+++ VideoGames/Juegos/models.py
@@ -6,7 +6,7 @@ class creator(models.Model):
 			('F','Female'),
 			('M','Male'),
 		)
-    name=models.CharField(max_lenght=25,help_text='Nombre del creador ')
+   	name=models.CharField(max_lenght=25,help_text='Nombre del creador ')
 	birthday=models.DateField(help_text='Fecha de nacimiento')
 	genere=models.Charfield(max_lenght=1, help_text='Coloque el sexo del creador' choices=generes)
 
@@ -15,20 +15,20 @@ class company(models.Model):
 
 	
 class games(models.Model):
-    """
-    almacena los juegos mediante el equipo de
-    administracion
-    """
-    title=models.CharField(max_lenght=30, help_text='Titulo del videojuego ')
-    creatores=models.ForeignKey(creator,help_text='Nombre del o los creadores ')
-    synopsis=models.TextField(max_lenght=300, help_text='Historia del Videojuego ')
-    pubdate=models.DateTimeField(help_text='Fecha y hora de la puclicacion del videojuego ')
-    clasification=models.DecimalField(max_lenght=5, help_text='Clasificacion del videojuego ')
-    genere=models.CharField(max_lenght=50, help_text='Genero del videojuego ')
-    console=models.CharField(max_lenght=50, help_text='Plataforma de videojuego ')
+	"""
+	almacena los juegos mediante el equipo de
+	administracion
+	"""
+	title=models.CharField(max_lenght=30, help_text='Titulo del videojuego ')
+	creatores=models.ForeignKey(creator,help_text='Nombre del o los creadores ')
+	synopsis=models.TextField(max_lenght=300, help_text='Historia del Videojuego ')
+	pubdate=models.DateTimeField(help_text='Fecha y hora de la puclicacion del videojuego ')
+	clasification=models.DecimalField(max_lenght=5, help_text='Clasificacion del videojuego ')
+	genere=models.CharField(max_lenght=50, help_text='Genero del videojuego ')
+	console=models.CharField(max_lenght=50, help_text='Plataforma de videojuego ')
 
 
 class review(models.Model):
-    review=models.IntegerField(max_lenght=100,help_text='Opiniones ')
+    	review=models.IntegerField(max_lenght=100,help_text='Opiniones ')
 	game=models.ForeignKey(games,help_text='Selecione el juego que desea hacer la sinopsis')
 	
