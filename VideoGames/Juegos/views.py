deb82e6f2b7da8526b79882480d89277e3303631
Edu Santos Paz <eduardo@edsantoshn.com>
2017-08-02 20:31:32 -0600

    Views

--- VideoGames/Juegos/views.py
+++ VideoGames/Juegos/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from datetime import date, datetime
+from django.views import generic
 
 # Create your views here.
+from .models import *
